8a165af2bbd1608eafead3993066fbcb348a1d1f
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-22 21:05:53 +0200

    feat(customers): add service for HTTP requests

--- src/app/services/customer-service.service.ts
+++ src/app/services/customer-service.service.ts
@@ -1,9 +1,92 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Customer } from '../models/customers';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class CustomerServiceService {
+@Injectable({ providedIn: 'root' })
+export class CustomerService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  private host: string = 'http://localhost:3000/';
+
+  private urls = {
+    getAllCustomers: 'getCustomers',
+    getCustomer: 'getCustomer/',
+    createCustomer: 'addCustomer',
+    updateCustomer: 'updateCustomer',
+    deleteCustomer: 'deleteCustomer/',
+    getAllActivity: 'getActivity',
+    getActivity: 'getActivity/:email',
+    saveActivity: 'saveActivity',
+    getAllMeetings: 'getMeetings',
+    getMeeting: 'getMeeting/:email',
+    createMeeting: 'addMeeting',
+    updateMeeting: 'updateMeeting',
+    deleteMeeting: 'deleteMeeting/:meetId'
+  }
+
+  public getAllCustomers$(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${this.host}${this.urls.getAllCustomers}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  public getCustomer$(email: string): Observable<Customer> {
+    return this.http.get<Customer>(`${this.host}${this.urls.getCustomer}${email}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  public createCustomer$(customer: Customer) {
+    return this.http.post(`${this.host}${this.urls.createCustomer}`, customer)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  public updateCustomer$(customer: Customer) {
+    return this.http.put(`${this.host}${this.urls.updateCustomer}`, customer)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  public deleteCustomer$(email: string) {
+    return this.http.delete(`${this.host}${this.urls.deleteCustomer}${email}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  public getAllActivity$(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${this.host}${this.urls.getAllCustomers}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  public getActivity$(email: string): Observable<Customer> {
+    return this.http.get<Customer>(`${this.host}${this.urls.getCustomer}${email}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  public saveActivity$(customer: Customer) {
+    return this.http.post(`${this.host}${this.urls.createCustomer}`, customer)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0)
+      console.error('An error ocurred: ' + error.error);
+    else
+      console.error(`Backend returned code ${error.status} error message is: `, error.error);
+    return throwError(() => new Error('An error occurred, please try again later'));
+  }
+
 }
