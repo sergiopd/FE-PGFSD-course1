4939caebd6e508c858cb9fd6278830b0a633bb23
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-24 21:28:08 +0200

    feat(view-meetings): add component to edit and view

--- src/app/meetings/view-all-meetings/view-all-meetings.component.ts
+++ src/app/meetings/view-all-meetings/view-all-meetings.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormMeetingComponent } from '../form-meeting/form-meeting.component';
+import { MeetingService } from 'src/app/services/meeting-service.service';
+import { Meeting } from 'src/app/models/meetings';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-view-all-meetings',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-all-meetings.component.css']
 })
 export class ViewAllMeetingsComponent {
+  @ViewChild(FormMeetingComponent) meetingForm!: FormMeetingComponent;
 
+  public meeting: Meeting = new Meeting('', '', '', '', '', '', 0, 0);
+  public meetings: Meeting[] = [];
+
+  constructor(private meetingService: MeetingService) { }
+
+  public getMeetings() {
+    this.meetingService.getAllMeetings$().subscribe(meetingReply => {
+      this.meetings = meetingReply;
+    })
+  }
+
+  public getMeeting(email: string) {
+    this.meetingService.getMeeting$(email)
+      .subscribe((customerReply) => {
+        this.meetingForm.meeting = customerReply[0];
+      });
+    this.meetingForm.isEditMeeting = true;
+  }
+
+  public updateMeeting() {
+    this.meetingService.updateMeeting$(this.meetingForm.meeting).subscribe();
+  }
+
+  public deleteMeeting(email: string, form: NgForm) {
+    this.meetingService.deleteMeeting$(email).subscribe();
+    this.meetingForm.meeting = new Meeting('', '', '', '', '', '', 0, 0);
+    form.reset();
+  }
 }
