444744297504321d71fbf523576d128c3b78a850
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-24 21:25:57 +0200

    feat(create-meeting): add component to add meeting

--- src/app/meetings/create-meeting/create-meeting.component.ts
+++ src/app/meetings/create-meeting/create-meeting.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormMeetingComponent } from '../form-meeting/form-meeting.component';
+import { NgForm } from '@angular/forms';
+import { MeetingService } from 'src/app/services/meeting-service.service';
+import { Meeting } from 'src/app/models/meetings';
 
 @Component({
   selector: 'app-create-meeting',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-meeting.component.css']
 })
 export class CreateMeetingComponent {
+  @ViewChild(FormMeetingComponent) meetingForm!: FormMeetingComponent;
 
+  constructor(private meetingService: MeetingService) { }
+
+  public addMeeting(form: NgForm) {
+    this.meetingService.createMeeting$(this.meetingForm.meeting).subscribe();
+    this.meetingForm.meeting = new Meeting('', '', '', '', '', '', 0, 0);
+    form.reset();
+  }
 }
