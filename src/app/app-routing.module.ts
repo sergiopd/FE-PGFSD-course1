0c6eb12f8ed6595073a1dbb5853183141e589f12
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-24 19:55:02 +0200

    refactor: remove unnecessary components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
-import { EditCustomerComponent } from './customers/edit-customer/edit-customer.component';
 import { CreateMeetingComponent } from './meetings/create-meeting/create-meeting.component';
-import { ViewAllCustomersComponent } from './customers/view-all-customers/view-all-customers.component';
 import { ViewAllActivityComponent } from './customers/view-all-activity/view-all-activity.component';
-import { EditMeetingComponent } from './meetings/edit-meeting/edit-meeting.component';
 import { ViewAllMeetingsComponent } from './meetings/view-all-meetings/view-all-meetings.component';
+import { ViewCustomersComponent } from './customers/view-customers/view-customers.component';
 
 const routes: Routes = [
   {
@@ -19,28 +17,16 @@ const routes: Routes = [
     component: CreateCustomerComponent,
   },
   {
-    path: 'meeting',
-    component: CreateMeetingComponent
-  },
-  {
-    path: 'customer/edit',
-    component: EditCustomerComponent,
-  },
-  {
     path: 'customer/view',
-    component: ViewAllCustomersComponent
+    component: ViewCustomersComponent
   },
   {
     path: 'activity/view',
     component: ViewAllActivityComponent
   },
   {
-    path: 'meeting/add',
+    path: 'meeting',
     component: CreateMeetingComponent
-  },
-  {
-    path: 'meeting/edit',
-    component: EditMeetingComponent
   },
   {
     path: 'meetings/view',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FormCustomerComponent } from './customers/form-customer/form-customer.component';
-import { EditCustomerComponent } from './customers/edit-customer/edit-customer.component';
-import { ViewAllCustomersComponent } from './customers/view-all-customers/view-all-customers.component';
 import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
-import { CreateMeetingComponent } from './meetings/create-meeting/create-meeting.component';
-import { EditMeetingComponent } from './meetings/edit-meeting/edit-meeting.component';
-import { ViewAllMeetingsComponent } from './meetings/view-all-meetings/view-all-meetings.component';
+import { ViewCustomersComponent } from './customers/view-customers/view-customers.component';
 import { ViewAllActivityComponent } from './customers/view-all-activity/view-all-activity.component';
-import { HttpClientModule } from '@angular/common/http';
+import { CreateMeetingComponent } from './meetings/create-meeting/create-meeting.component';
+import { ViewAllMeetingsComponent } from './meetings/view-all-meetings/view-all-meetings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormCustomerComponent,
-    EditCustomerComponent,
-    ViewAllCustomersComponent,
     CreateCustomerComponent,
+    ViewCustomersComponent,
+    ViewAllActivityComponent,
     CreateMeetingComponent,
-    EditMeetingComponent,
     ViewAllMeetingsComponent,
-    ViewAllActivityComponent
   ],
   imports: [
     BrowserModule,
