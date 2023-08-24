aac0b073096861e795bbbaa4d69d1176435e759c
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-24 21:26:56 +0200

    improve model for meetings

--- src/app/models/meetings.ts
+++ src/app/models/meetings.ts
@@ -1,10 +1,22 @@
-export interface Meeting {
+export class Meeting {
   customerName: string;
   date: Date | string;
   email: string;
   endAt: string;
-  meetID?: number;
-  meetingAttendees: number;
   meetTitle: string;
   startAt: string;
+  meetingAttendees: number;
+  meetID: number;
+  constructor(
+    customerName: string, date: string, email: string, endAt: string, meetTitle: string, startAt: string, meetingAttendees: number, meetID: number = 0
+  ) {
+    this.customerName = customerName;
+    this.date = date;
+    this.email = email;
+    this.endAt = endAt;
+    this.meetingAttendees = meetingAttendees;
+    this.meetTitle = meetTitle;
+    this.startAt = startAt;
+    this.meetID = meetID;
+  }
 }
