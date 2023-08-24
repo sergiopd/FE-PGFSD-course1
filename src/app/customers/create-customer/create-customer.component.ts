3b54bdddf62eb1d7277ce42c32171328b1ec78d1
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-24 19:50:04 +0200

    feat(create-cutomer):  add form component to reuse

--- src/app/customers/create-customer/create-customer.component.ts
+++ src/app/customers/create-customer/create-customer.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormCustomerComponent } from '../form-customer/form-customer.component';
 import { CustomerService } from 'src/app/services/customer-service.service';
+import { NgForm } from '@angular/forms';
+import { Customer } from 'src/app/models/customers';
 
 @Component({
   selector: 'app-create-customer',
@@ -12,8 +14,9 @@ export class CreateCustomerComponent {
 
   constructor(private customerService: CustomerService) { }
 
-  public addCustomer() {
+  public addCustomer(form: NgForm) {
     this.customerService.createCustomer$(this.customerForm.customer).subscribe();
+    this.customerForm.customer = new Customer('', '', '', '');
+    form.reset();
   }
-
 }
