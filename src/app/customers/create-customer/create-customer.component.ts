7fc433c5b950f5fc8dc2fb166c6851750b931d9a
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-25 21:17:12 +0200

    feat(create-customer): add unsubscribe

--- src/app/customers/create-customer/create-customer.component.ts
+++ src/app/customers/create-customer/create-customer.component.ts
@@ -1,21 +1,28 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { FormCustomerComponent } from '../form-customer/form-customer.component';
 import { CustomerService } from 'src/app/services/customer-service.service';
 import { NgForm } from '@angular/forms';
 import { Customer } from 'src/app/models/customers';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-customer',
   templateUrl: './create-customer.component.html',
   styleUrls: ['./create-customer.component.css']
 })
-export class CreateCustomerComponent {
+export class CreateCustomerComponent implements OnDestroy {
   @ViewChild(FormCustomerComponent) customerForm!: FormCustomerComponent;
+
+  private subscription = new Subscription();
 
   constructor(private customerService: CustomerService) { }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   public addCustomer(form: NgForm) {
-    this.customerService.createCustomer$(this.customerForm.customer).subscribe();
+    this.subscription = this.customerService.createCustomer$(this.customerForm.customer).subscribe();
     this.customerForm.customer = new Customer('', '', '', '');
     form.reset();
   }
