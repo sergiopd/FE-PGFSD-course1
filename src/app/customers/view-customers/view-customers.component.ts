ce7092432c3477a9679c39168b2c5f14f888c02b
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-25 21:17:27 +0200

    feat(view-customers): add unsubscribe

--- src/app/customers/view-customers/view-customers.component.ts
+++ src/app/customers/view-customers/view-customers.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Customer } from 'src/app/models/customers';
 import { CustomerService } from 'src/app/services/customer-service.service';
 import { FormCustomerComponent } from '../form-customer/form-customer.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-view-customers',
   templateUrl: './view-customers.component.html',
   styleUrls: ['./view-customers.component.css']
 })
-export class ViewCustomersComponent implements OnInit {
+export class ViewCustomersComponent implements OnInit, OnDestroy {
   @ViewChild(FormCustomerComponent) customerForm!: FormCustomerComponent;
 
   public customer: Customer = new Customer('', '', '', '');
   public customers: Customer[] = [];
+  private subscriptions = new Subscription();
 
   constructor(private customerService: CustomerService) { }
 
@@ -21,26 +23,34 @@ export class ViewCustomersComponent implements OnInit {
     this.getCustomers();
   }
 
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
   public getCustomers() {
-    this.customerService.getAllCustomers$().subscribe(customersReply => {
-      this.customers = customersReply;
-    })
+    this.subscriptions.add(this.customerService.getAllCustomers$()
+      .subscribe(
+        customersReply => {
+          this.customers = customersReply;
+        })
+    );
   }
 
   public getCustomer(email: string) {
-    this.customerService.getCustomer$(email)
+    this.subscriptions.add(this.customerService.getCustomer$(email)
       .subscribe((customerReply) => {
         this.customerForm.customer = customerReply[0];
-      });
+      })
+    );
     this.customerForm.isEditCustomer = true;
   }
 
   public updateCustomer() {
-    this.customerService.updateCustomer$(this.customerForm.customer).subscribe();
+    this.subscriptions.add(this.customerService.updateCustomer$(this.customerForm.customer).subscribe());
   }
 
   public deleteCustomer(email: string, form: NgForm) {
-    this.customerService.deleteCustomer$(email).subscribe();
+    this.subscriptions.add(this.customerService.deleteCustomer$(email).subscribe());
     this.customerForm.customer = new Customer('', '', '', '');
     form.reset();
   }
