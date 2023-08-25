a364b0602ddd36e9eaef2f09adc2e573bba3bc77
Sergio Perez <sergio.perez.extern@ista.com>
2023-08-25 21:21:29 +0200

    feat(create-meeting): add unsubscribe

--- src/app/meetings/create-meeting/create-meeting.component.ts
+++ src/app/meetings/create-meeting/create-meeting.component.ts
@@ -1,21 +1,29 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnDestroy } from '@angular/core';
 import { FormMeetingComponent } from '../form-meeting/form-meeting.component';
 import { NgForm } from '@angular/forms';
 import { MeetingService } from 'src/app/services/meeting-service.service';
 import { Meeting } from 'src/app/models/meetings';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-meeting',
   templateUrl: './create-meeting.component.html',
   styleUrls: ['./create-meeting.component.css']
 })
-export class CreateMeetingComponent {
+export class CreateMeetingComponent implements OnDestroy {
   @ViewChild(FormMeetingComponent) meetingForm!: FormMeetingComponent;
+
+  private subscription = new Subscription();
 
   constructor(private meetingService: MeetingService) { }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   public addMeeting(form: NgForm) {
-    this.meetingService.createMeeting$(this.meetingForm.meeting).subscribe();
+    this.meetingForm.meeting.meetDate = this.meetingForm.date;
+    this.subscription = this.meetingService.createMeeting$(this.meetingForm.meeting).subscribe();
     this.meetingForm.meeting = new Meeting('', '', '', '', '', '', 0, 0);
     form.reset();
   }
